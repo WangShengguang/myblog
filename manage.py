601fa7cc26998f06922169be04f9cb57dc4931f7
unknown <>
2016-06-04 15:52:03 +0800

    'prod'

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 
 from app import create_app, db
-from app.models import User, Role, Post,Category
+from app.models import User, Role, Post, Category
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -13,24 +13,25 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Post=Post,Category=Category)
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post, Category=Category)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def deploy():
     from flask.ext.migrate import upgrade
     from app.models import Role, User, Post
 
-    #upgrade()
+    # upgrade()
     Role.insert_roles()
     Category.insert_categorys()
     User.add_self_follows()
 
-    User.generate_fake(100)
-    Post.generate_fake(100)
+    # User.generate_fake(100)
+    # Post.generate_fake(100)
 
 
 @manager.command
