0648e7d6927ccdb958294c38dcfdbb5df0ac3742
unknown <>
2016-06-05 17:52:21 +0800

    'index'

--- app/models.py
+++ app/models.py
@@ -333,7 +333,7 @@ class Category(db.Model):
 
     @staticmethod
     def insert_categorys():
-        categorys = ['Python', 'Flask', 'MySQL', '随笔'.decode('utf-8'),'诗词'.decode('utf-8')]
+        categorys = ['Python', 'Flask', 'MySQL', 'Linux','随笔'.decode('utf-8'),'诗词'.decode('utf-8')]
         for name in categorys:
             category = Category.query.filter_by(name=name).first()
             if category is None:
